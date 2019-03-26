1cdaa8b105e0a7c4cc29a0146ea1d26c5f573180
Einar W. Høst <einarwh@gmail.com>
2019-03-26 16:44:01 +0100

    times shades

--- src/Client/data/Shades.fs
+++ src/Client/data/Shades.fs
@@ -10,15 +10,17 @@ let blank : Picture = fun _ -> []
 
 (*
 
-let turn (p : Picture) = turnLens >> p 
+let turn (p : Picture) = turnLens >> p
 
-let flip (p : Picture) = flipLens >> p 
+let flip (p : Picture) = flipLens >> p
 
-let toss (p : Picture) = tossLens >> p 
+let toss (p : Picture) = tossLens >> p
 
-let aboveRatio (m : int) (n : int) (p1 : Picture) (p2 : Picture) = 
+let rec times n fn = if n < 1 then id else fn >> times (n - 1) fn
+
+let aboveRatio (m : int) (n : int) (p1 : Picture) (p2 : Picture) =
   fun lens ->
-    let factor = float m / float (m + n) 
+    let factor = float m / float (m + n)
     let b1, b2 = splitLensVertically factor lens
     p1 b1 @ p2 b2
 
@@ -26,7 +28,7 @@ let above = aboveRatio 1 1
 
 let besideRatio (m : int) (n : int) (p1 : Picture) (p2 : Picture) =
   fun lens ->
-    let factor = float m / float (m + n) 
+    let factor = float m / float (m + n)
     let b1, b2 = splitLensHorizontally factor lens
     p1 b1 @ p2 b2
 
@@ -36,26 +38,26 @@ let rec row = function
   | [] -> blank
   | [p] -> p
   | [p1; p2] -> beside p1 p2
-  | p :: rest -> besideRatio 1 (List.length rest) p (row rest) 
+  | p :: rest -> besideRatio 1 (List.length rest) p (row rest)
 
 let rec column = function
   | [] -> blank
   | [p] -> p
   | [p1; p2] -> above p1 p2
-  | p :: rest -> aboveRatio 1 (List.length rest) p (row rest) 
-   
-let quartet nw ne sw se = 
+  | p :: rest -> aboveRatio 1 (List.length rest) p (row rest)
+
+let quartet nw ne sw se =
   above (beside nw ne) (beside sw se)
 
 let nonet nw nm ne mw mm me sw sm se =
   let row w m e = besideRatio 1 2 w (beside m e)
   aboveRatio 1 2
     (row nw nm ne)
-    (above 
+    (above
       (row mw mm me)
       (row sw sm se))
-      
-let over p1 p2 = 
+
+let over p1 p2 =
   fun lens ->
     p1 lens @ p2 lens
 
@@ -63,7 +65,7 @@ let over p1 p2 =
 let rehue p =
   rehueLens >> p
 
-let ttile hueN hueE fish = 
+let ttile hueN hueE fish =
   let fishN = fish |> toss |> flip
   let fishE = fishN |> turn |> turn |> turn
   over fish (over (hueN fishN) (hueE fishE))
@@ -72,7 +74,7 @@ let ttile1 = ttile rehue (rehue >> rehue)
 
 let ttile2 = ttile (rehue >> rehue) rehue
 
-let utile hueN hueW hueS hueE fish = 
+let utile hueN hueW hueS hueE fish =
   let fishN = fish |> toss |> flip
   let fishW = turn fishN
   let fishS = turn fishW
@@ -84,9 +86,9 @@ let utile1 = utile (rehue >> rehue) id (rehue >> rehue) id
 let utile2 = utile id (rehue >> rehue) rehue (rehue >> rehue)
 let utile3 = utile (rehue >> rehue) id rehue id
 
-let side tt hueSW hueSE n fish = 
+let side tt hueSW hueSE n fish =
   let t = tt fish
-  let rec recur n = 
+  let rec recur n =
     let r = if n = 1 then blank else recur (n - 1)
     quartet r r (t |> turn |> hueSW) (t |> hueSE)
   recur n
@@ -95,11 +97,11 @@ let sideNS = side ttile1 id rehue
 
 let sideEW = side ttile2 (rehue >> rehue) rehue
 
-let corner ut side1 side2 n fish = 
+let corner ut side1 side2 n fish =
   let u = ut fish
-  let rec fn n = 
-    let c, ne, sw = 
-      if n = 1 then blank, blank, blank 
+  let rec fn n =
+    let c, ne, sw =
+      if n = 1 then blank, blank, blank
                else fn (n - 1), side1 (n - 1) fish, side2 (n - 1) fish
     quartet c ne (sw |> turn) u
   fn n
@@ -107,7 +109,7 @@ let corner ut side1 side2 n fish =
 let cornerNWSE = corner utile3 sideNS sideEW
 let cornerNESW = corner utile2 sideEW sideNS
 
-let squareLimit n fish = 
+let squareLimit n fish =
   let cornerNW = cornerNWSE n fish
   let cornerSW = cornerNESW n fish |> turn
   let cornerSE = cornerNW |> turn |> turn
@@ -117,8 +119,8 @@ let squareLimit n fish =
   let sideS = sideN |> turn |> turn
   let sideE = sideW |> turn |> turn
   let center = utile1 fish
-  nonet cornerNW sideN cornerNE  
+  nonet cornerNW sideN cornerNE
         sideW center sideE
         cornerSW sideS cornerSE
-        
+
 *)
